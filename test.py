14e7ade6d268ce3baa533c6390478d95a435ac35
luca-5tti <luca.cinquetti@studenti.unimi.it>
2025-02-07 15:16:46 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
 import cv2
-print(cv2.__version__)
+print("cv2 version:", cv2.__version__)
+
+import face_recognition
+print("face recognition version:", face_recognition.__version__)
+
+import dlib
+print("dlib version:", dlib.__version__)
+
+import numpy as np
+print("np verion:", np.__version__)
+
+print("Tutte le importazioni completate con successo")
